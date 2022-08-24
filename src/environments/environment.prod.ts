ed4161dc79e88f0931af09369f8af5264d8fab88
suni.betasoft@gmail.com <kumarsunil.bwn@gmail.com>
2022-08-24 15:44:31 +0530

    Fixed Ipad issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 const siteURL = 'https://dev-sunil.peapodjewelry.com/';
 export const environment = {
   production: true,
-  baseURL: `${siteURL}wp-json/`,
+  baseURL: `${siteURL}wp-json/jwt-auth/v1/`,
   siteURL,
 };
